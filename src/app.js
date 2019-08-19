6184512130e6aafa87e9ff50145e37ee865adfda
prasad <you@example.com>
2019-08-20 01:03:38 +0530

    Add few design changes

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.static(publicDirectorPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather app',
+        title: 'Weather App',
         name : 'Prasad Gorle'
     })
 })
